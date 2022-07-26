b6b620fa8a1d2a8135a3e5b9afe2585c17edaa53
wcphaha <1683168521@qq.com>
2022-07-26 20:59:59 +0800

    finish graph2vec

--- GraphPyramidPooling.py
+++ GraphPyramidPooling.py
@@ -1,47 +1,68 @@
 import networkx as nx
 import numpy as np
-import scipy.io as sio
+from sklearn.preprocessing import MinMaxScaler
 
 
 def graph2vec(G):
     ranking_vec = node_attr_ranking(G)
     pooling_vector = pyramid_pooling(ranking_vec)
-    pass
+    print(pooling_vector.shape)
+    return pooling_vector
 
 
 def node_attr_ranking(G, label=None):
     ranking_vec = []
     if label is None:
-        label = ['degree', 'betweenness']
+        label = ['degree', 'betweenness', 'clustering']
     for i in label:
         if i == 'degree':
             ranking_vec.append(node_degree_ranking(G))
         if i == 'betweenness':
             ranking_vec.append(node_betweenness_ranking(G))
-    # print(ranking_vec)
+        if i == 'clustering':
+            ranking_vec.append(node_clustering_ranking(G))
     return np.array(ranking_vec)
 
 
 def node_degree_ranking(G):
+    scaler = MinMaxScaler()
     ranking_nodes = sorted(nx.degree(G), key=lambda x: x[1], reverse=True)
     ranking_degree = [n[1] for n in ranking_nodes]
-    return np.array(ranking_degree)
+    ranking_degree = scaler.fit_transform(np.array(ranking_degree).reshape((len(ranking_degree), 1)))
+    return ranking_degree.flatten()
 
 
 def node_betweenness_ranking(G):
-    betweenness = nx.betweenness_centrality(G)
-    print(betweenness[0])
-    node_betweenness = [i for i in betweenness]
-    ranking_betweenness = sorted(node_betweenness, reverse=True)
-    print(ranking_betweenness)
-    return ranking_betweenness
+    scaler = MinMaxScaler()
+    ranking_nodes = sorted(nx.betweenness_centrality(G).items(), key=lambda x: x[1], reverse=True)
+    ranking_betweenness = [n[1] for n in ranking_nodes]
+    ranking_betweenness = scaler.fit_transform(np.array(ranking_betweenness).reshape((len(ranking_betweenness), 1)))
+    return ranking_betweenness.flatten()
+
+
+def node_clustering_ranking(G):
+    ranking_nodes = sorted(nx.clustering(G).items(), key=lambda x: x[1], reverse=True)
+    ranking_clustering = [n[1] for n in ranking_nodes]
+    return ranking_clustering
 
 
 def pyramid_pooling(vec, pooling_sizes=None):
     if pooling_sizes is None:
         pooling_sizes = [1, 2, 4, 8, 16]
-    pass
+    pooling_vec = []
+    for v in vec:
+        for s in pooling_sizes:
+            pooling_vec = np.concatenate([pooling_vec, pooling(v, s)])
+    return pooling_vec
 
 
-def pooling(pooling_size, max_pooling=True):
-    pass
+def pooling(vec, size, max_pooling=True):
+    single_size_pooling_vec = []
+    length = len(vec)
+    divid = int(np.floor(length / size))
+    for i in range(size):
+        if max_pooling:
+            single_size_pooling_vec.append(np.max(vec[i * divid:(i + 1) * divid]))
+        else:
+            single_size_pooling_vec.append(np.mean(vec[i * divid:(i + 1) * divid]))
+    return single_size_pooling_vec
